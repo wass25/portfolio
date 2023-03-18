3b1175e004e305071cedba706adebbeb0beb8d36
Wassel Azzouz <113088456+wass25@users.noreply.github.com>
2023-03-18 22:23:22 +0100

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,14 +6,14 @@ import darkmode from "../components/darkmode.vue"
 	<nav id="nav" class="sm:container sm:mx-auto">
 
 		<div class="m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none">
-			<a href="/"
+			<router-link to="/"
 				class="router-link-active router-link-exact-active font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300 sm:mx-4 mb-2 sm:py-2">Accueil
-			</a>
-			<a href="../pages/contact.vue"
-				class="router-link-active router-link-exact-active font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300 sm:mx-4 mb-2 sm:py-2">Contact</a>
-			<a href="../pages/apropos.vue"
+			</router-link>
+			<router link to="contact"
+				class="router-link-active router-link-exact-active font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300 sm:mx-4 mb-2 sm:py-2">Contact</router-link>
+			<router-link to="apropos"
 				class="router-link-active router-link-exact-active font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300 sm:mx-4 mb-2 sm:py-2">A
-				propos</a>
+				propos</router-link>
 
 
 
