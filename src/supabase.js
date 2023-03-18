7a38a074ff570a33c430cf615b6acb479667b095
Wassel Azzouz <113088456+wass25@users.noreply.github.com>
2023-03-18 22:10:13 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import { ref } from "@vue/reactivity";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://bkqcxizocgzjxbjkjiuz.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrcWN4aXpvY2d6anhiamtqaXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzczNjMzMTYsImV4cCI6MTk5MjkzOTMxNn0.QyIzP49bMvKbTQ-mlZ_lqpy1nr_MxHQuo-iwV8-EVUc" ;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
